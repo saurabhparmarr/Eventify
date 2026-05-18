217758fccd065983c4aa07cc27434bfc2c70c6a0
Saurabh Singh <saurabhsinpar@gmail.com>
2026-05-18 10:01:52 +0530

    add root route

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://eventify-0f8e.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
