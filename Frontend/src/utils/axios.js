5ef86cf73aab37158a0cf12bcd2897492174a993
Saurabh Singh <saurabhsinpar@gmail.com>
2026-05-18 10:08:33 +0530

    update production backend url

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://eventify-0f8e.onrender.com",
+  baseURL: "https://eventify-0f8e.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
