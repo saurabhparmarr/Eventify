e6f17446812ccfd8cea274fdcd6918073659554e
Saurabh Singh <saurabhsinpar@gmail.com>
2026-05-20 02:26:58 +0530

    force ipv4 for gmail smtp

--- Backend/utils/email.js
+++ Backend/utils/email.js
@@ -3,10 +3,13 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+
+
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
